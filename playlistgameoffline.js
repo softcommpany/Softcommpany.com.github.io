67345a29811fdbc4ff7773d3e5b5186bfe44d61f
Alexander <softcommpany@gmail.com>
2023-09-22 14:29:08 -0300

    UpdateWeb
    none

---
+++ playlistgameoffline.js
@@ -0,0 +1,43 @@
+var solicitarurl = window.location.href;
+
+if (solicitarurl == "file:///C:/Users/Anonimus/Documents/GitHub/Softcommpany.com/pagewindowsgame.html")
+{
+	function GameInfromacionWindows(Icono, Name, LinkGame, Public, Presio)
+	{
+		document.getElementById('Allgamebox').innerHTML += 
+
+		`<section class="GameBox">
+            <img class="IconoGame" src="${Icono}">
+            <img class="PublicCheck" src="Texture/${Public}.png">
+            <a class="gamelink" href="${LinkGame}">${Name}</a>
+         </section>`;
+	}
+
+	//resgistros de definiciongame para windows/
+
+    GameInfromacionWindows("Texture/IconsGame/FortniqueGame.png", "Fortnique", "https://drive.google.com/file/d/1NSnzqK90a287b3LDSjGeOUQpfbegmNHU/view?usp=drive_link", "publico")
+    GameInfromacionWindows("Texture/IconsGame/gameiconodefault.png", "sin definir", "", "privado")
+
+
+
+} else 
+{
+	if (solicitarurl == "file:///C:/Users/Anonimus/Documents/GitHub/Softcommpany.com/pageandroidgame.html") 
+	{
+		function GameInformacionAndroid(Icono, Name, LinkGame, Public, Presio) 
+		{
+		    document.getElementById('Allgamebox').innerHTML += 
+
+		    `<section class="GameBox">
+                <img class="IconoGame" src="${Icono}">
+                <img class="PublicCheck" src="Texture/${Public}.png">
+                <a class="gamelink" href="${LinkGame}">${Name}</a>
+            </section>`;
+		}
+
+		//registros de definiciongame para android/
+		GameInformacionAndroid("Texture/IconsGame/FortniqueGame.png", "Fortnique", "https://drive.google.com/file/d/1GXlcgvx1HqvmdgZtxUUr10g11VM6ygLo/view?usp=drive_link", "publico")
+
+
+	} else {document.getElementById('Allgamebox').innerHTML += "NO HAY CONTENIDO PARA MOSTRAR HAY, PUEDE QUE NO AN PUBLICADO NADA AUN."}
+}
